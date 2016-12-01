1b455c20babe83722020030f97fe154319deb7ec
Jessica Appelbaum <jess.appelbaum@gmail.com>
2016-12-01 14:44:42 -0500

    Added dropdown

--- app.py
+++ app.py
@@ -12,5 +12,20 @@ def about():
 	return render_template(
 		'about.html',**locals())
 
+@app.route("/lettera")
+def lettera():
+	return render_template(
+		'lettera.html',**locals())
+
+@app.route("/letterb")
+def letterb():
+	return render_template(
+		'letterb.html',**locals())
+
+@app.route("/letterc")
+def letterc():
+	return render_template(
+		'letterc.html',**locals())
+
 if __name__ == "__main__":
 	app.run()
